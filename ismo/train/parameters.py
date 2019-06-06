4a1719cbca08a5cf41f34c713b909e3340e315d0
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-06-06 16:49:10 +0200

    added retrainings

--- ismo/train/parameters.py
+++ ismo/train/parameters.py
@@ -1,30 +1,27 @@
 import json
+import typing
 
-class Parameters(object):
-    def __init__(self, *,
-                 epochs,
-                 optimizer,
-                 loss,
-                 should_use_early_stopping,
-                 early_stopping_patience,
-                 learning_rate
-                 ):
 
-        self.optimizer = optimizer
-        self.epochs = epochs
-        self.loss = loss
-        self.should_use_early_stopping = should_use_early_stopping
-        self.early_stopping_patience = early_stopping_patience
-        self.learning_rate = learning_rate
+class Parameters(typing.NamedTuple):
+    epochs: int
+    optimizer: str
+    loss: str
+    should_use_early_stopping: bool
+    early_stopping_patience: int
+    learning_rate: float
+    retrainings: int
 
 
 def parameters_from_simple_config_file(configuration_filename):
     with open(configuration_filename) as f:
         config = json.load(f)
 
+
+
         return Parameters(epochs=config['epochs'],
                           optimizer=config['optimizer'],
                           loss=config['loss'],
                           should_use_early_stopping=config['should_use_early_stopping'],
                           early_stopping_patience=config['early_stopping_patience'],
-                          learning_rate=config['learning_rate'])
+                          learning_rate=config['learning_rate'],
+                          retrainings=config['retrainings'])
--- ismo/train/simple_trainer.py
+++ ismo/train/simple_trainer.py
@@ -2,27 +2,57 @@ from ismo.train import Parameters
 import ismo.train.optimizers
 import h5py
 import keras.callbacks
+import copy
+import numpy as np
 
 
 class SimpleTrainer(object):
 
     def __init__(self, *, training_parameters: Parameters,
                  model):
-        model.compile(optimizer=ismo.train.optimizers.create_optimizer(training_parameters.optimizer,
-                                                                      training_parameters.learning_rate),
-                      loss=training_parameters.loss)
+
+        self.retrainings = training_parameters.retrainings
+
+        self.optimizer = training_parameters.optimizer
+        self.learning_rate = training_parameters.learning_rate
 
         self.epochs = training_parameters.epochs
 
         self.model = model
 
+        self.loss = training_parameters.loss
+
         self.callbacks = []
         if training_parameters.should_use_early_stopping:
-            self.callbacks.append(keras.callbacks.EarlyStopping(monitor='loss', patience=training_parameters.early_stopping_patience))
+            self.callbacks.append(
+                keras.callbacks.EarlyStopping(monitor='loss', patience=training_parameters.early_stopping_patience))
 
     def fit(self, parameters, values):
-        self.model.fit(parameters, values, batch_size=parameters.shape[0],
-                  epochs=self.epochs, verbose=0, callbacks=self.callbacks)
+        best_loss = None
+        for retraining_number in range(self.retrainings):
+            self.__reinitialize(self.model)
+
+            hist = self.model.fit(parameters, values, batch_size=parameters.shape[0],
+                                  epochs=self.epochs, verbose=0, callbacks=self.callbacks)
+
+            loss = hist.history['loss'][-1]
+            if best_loss is None or loss < best_loss:
+                best_weights = copy.deepcopy(self.model.get_weights())
+
+        self.model.set_weights(best_weights)
 
     def save_to_file(self, outputname):
         self.model.save(outputname)
+
+    def __reinitialize(self, model):
+        weights = model.get_weights()
+        weights_new = []
+
+        for array in weights:
+            weights_new.append(np.random.uniform(0, 1, array.shape))
+
+        model.set_weights(weights_new)
+
+        model.compile(optimizer=ismo.train.optimizers.create_optimizer(self.optimizer,
+                                                                       self.learning_rate),
+                      loss=self.loss)
